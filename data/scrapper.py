2f4b202da68e5f0f55efe5d5ad17e66227b6bf52
alejandroMorales03 <94929338+alejandroMorales03@users.noreply.github.com>
2023-11-06 00:36:28 -0500

    Update scrapper.py to scrape every hour from the terminal

--- data/scrapper.py
+++ data/scrapper.py
@@ -4,27 +4,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
 from datetime import datetime
+import schedule
+import time
 
-driver = webdriver.Safari()
+def scrape_and_save_data():
+    driver = webdriver.Safari()
+    driver.get('https://parking.fiu.edu')
 
-# Navigate to the webpage with JavaScript content
-driver.get('https://parking.fiu.edu')
+    try:
+        wait = WebDriverWait(driver, 10)
+        element_present = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="parking-widget"]/div/div/ul/li[6]/div/div/div[2]/div/div/strong')))
 
-# Wait for a specific element to be present (adjust timeout as needed)
-wait = WebDriverWait(driver, 10)
-element_present = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="parking-widget"]/div/div/ul/li[6]/div/div/div[2]/div/div/strong')))
+        iterations = 6
+        row_data = [datetime.now().strftime('%H:%M:%S')]
 
-# Once the element is present, you can scrape it
-iterations = 6
-with open("location_data.csv", "a") as file:
-    csv_writer = csv.writer(file, delimiter=',')  # Specify the delimiter as a comma
-    row_data = [datetime.now().strftime('%Y-%m-%d %H:%M:%S')]  # Add the current time
-    for i in range(iterations):
-        element = driver.find_element(By.XPATH, '//*[@id="parking-widget"]/div/div/ul/li[' + str(i+1) + ']/div/div/div[2]/div/div/strong')
-        element_text = element.text
-        row_data.append(element_text)  # Add the element's text to the row_data list
-    csv_writer.writerow(row_data)  # Write the row_data list as a row
-    file.close()
+        with open("location_data.csv", "a") as file:
+            csv_writer = csv.writer(file, delimiter=',')
+            for i in range(iterations):
+                element = driver.find_element(By.XPATH, '//*[@id="parking-widget"]/div/div/ul/li[' + str(i + 1) + ']/div/div/div[2]/div/div/strong')
+                element_text = element.text
+                row_data.append(element_text)
+            csv_writer.writerow(row_data)
+            file.close()
 
-# Close the WebDriver when done
-driver.quit()
+    except Exception as e:
+        print(f"Error: {e}")
+
+    driver.quit()
+
+# Schedule the scraping task to run every hour
+schedule.every(1).hour.do(scrape_and_save_data)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
