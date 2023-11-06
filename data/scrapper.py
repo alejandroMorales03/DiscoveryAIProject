a7151c336edc329f9e24dc78c30195b8cf7a7cfc
alejandroMorales03 <94929338+alejandroMorales03@users.noreply.github.com>
2023-11-06 00:03:11 -0500

    Update scrapper.py

--- data/scrapper.py
+++ data/scrapper.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
-
+from datetime import datetime
 
 driver = webdriver.Safari()
 
@@ -17,14 +17,14 @@ element_present = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id=
 # Once the element is present, you can scrape it
 iterations = 6
 with open("location_data.csv", "a") as file:
+    csv_writer = csv.writer(file, delimiter=',')  # Specify the delimiter as a comma
+    row_data = [datetime.now().strftime('%Y-%m-%d %H:%M:%S')]  # Add the current time
     for i in range(iterations):
         element = driver.find_element(By.XPATH, '//*[@id="parking-widget"]/div/div/ul/li[' + str(i+1) + ']/div/div/div[2]/div/div/strong')
         element_text = element.text
-        file.write('\t\t\t\t' + element_text)
+        row_data.append(element_text)  # Add the element's text to the row_data list
+    csv_writer.writerow(row_data)  # Write the row_data list as a row
     file.close()
-
-# Print or process the scraped data
-
 
 # Close the WebDriver when done
 driver.quit()
